7772e0184d2b283579b8999ae2a3dbf841978a6d
JayB Productions <jaybgameproductions@gmail.com>
2022-01-05 17:02:13 -0500

    initial commit

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="grid gap-20 w-screen h-40 grid-flow-col pt-10 pl-40 pr-40 border-t-2 mt-10 justify-between">
+        <p>Resources</p>
+        <p>About Us </p>
+        <p>Contact Info</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
+};
