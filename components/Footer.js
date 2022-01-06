df6f18b90db17d88505305f4752077e9ce98f15a
JayB Productions <jaybgameproductions@gmail.com>
2022-01-05 20:51:52 -0500

    fixing padding on mobile

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="grid gap-20 w-screen h-40 grid-flow-col pt-10 pl-40 pr-40 border-t-2 mt-10 justify-between">
+      <div className="grid gap-20 w-screen h-40 grid-flow-col pt-10  border-t-2 mt-10 justify-between">
         <p>Resources</p>
         <p>About Us </p>
         <p>Contact Info</p>
