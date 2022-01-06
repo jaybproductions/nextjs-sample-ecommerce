0786503c0766db7e64eb9728039a8a1804154dd6
JayB Productions <jaybgameproductions@gmail.com>
2022-01-05 20:36:34 -0500

    fixing package.json

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
               <span className="block text-indigo-600 xl:inline">Online</span>
             </h1>
             <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-              A sample ecommcerce store to learn the ins and outs of nextjs and
+              A sample ecommcerce store to show the ins and outs of nextjs and
               tailwind css.
             </p>
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
